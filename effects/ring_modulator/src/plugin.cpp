cbf9147e522e07d6b0c529e6ce6c13178035f8c2
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-09-23 18:24:58 +0100

    ring modulator: add icon

--- effects/ring_modulator/src/plugin.cpp
+++ effects/ring_modulator/src/plugin.cpp
@@ -24,6 +24,8 @@ Plugin* g_plugin = nullptr;
 
 } // ring_modulator
 
+using namespace ring_modulator;
+
 blink_PluginInfo blink_get_plugin_info()
 {
 	blink_PluginInfo out = blink_PluginInfo();
@@ -39,79 +41,95 @@ blink_PluginInfo blink_get_plugin_info()
 
 blink_Error blink_init()
 {
-	if (ring_modulator::g_plugin) return blink_StdError_AlreadyInitialized;
+	if (g_plugin) return blink_StdError_AlreadyInitialized;
 
-	ring_modulator::g_plugin = new ring_modulator::Plugin();
+	g_plugin = new ring_modulator::Plugin();
 
 	return BLINK_OK;
 }
 
 blink_Error blink_terminate()
 {
-	if (!ring_modulator::g_plugin) return blink_StdError_NotInitialized;
+	if (!g_plugin) return blink_StdError_NotInitialized;
 
-	delete ring_modulator::g_plugin;
+	delete g_plugin;
 
 	return BLINK_OK;
 }
 
 blink_Error blink_stream_init(blink_SR SR)
 {
-	if (!ring_modulator::g_plugin) return blink_StdError_NotInitialized;
+	if (!g_plugin) return blink_StdError_NotInitialized;
 
-	ring_modulator::g_plugin->stream_init(SR);
+	g_plugin->stream_init(SR);
 
 	return BLINK_OK;
 }
 
 blink_EffectInstance blink_make_effect_instance()
 {
-	if (!ring_modulator::g_plugin) return blink_EffectInstance{ 0 };
+	if (!g_plugin) return blink_EffectInstance{ 0 };
 
-	return bind::effect_instance(ring_modulator::g_plugin->add_instance());
+	return bind::effect_instance(g_plugin->add_instance());
 }
 
 blink_Error blink_destroy_effect_instance(blink_EffectInstance instance)
 {
-	if (!ring_modulator::g_plugin) return blink_StdError_NotInitialized;
+	if (!g_plugin) return blink_StdError_NotInitialized;
 
 	const auto obj = (ring_modulator::Instance*)(instance.proc_data);
 
-	ring_modulator::g_plugin->destroy_instance(obj);
+	g_plugin->destroy_instance(obj);
 
 	return BLINK_OK;
 }
 
 int blink_get_num_groups()
 {
-	if (!ring_modulator::g_plugin) return 0;
+	if (!g_plugin) return 0;
 
-	return ring_modulator::g_plugin->get_num_groups();
+	return g_plugin->get_num_groups();
 }
 
 int blink_get_num_parameters()
 {
-	if (!ring_modulator::g_plugin) return 0;
+	if (!g_plugin) return 0;
 
-	return ring_modulator::g_plugin->get_num_parameters();
+	return g_plugin->get_num_parameters();
 }
 
 blink_Group blink_get_group(blink_Index index)
 {
-	return bind::group(ring_modulator::g_plugin->get_group(index));
+	return bind::group(g_plugin->get_group(index));
 }
 
 blink_Parameter blink_get_parameter(blink_Index index)
 {
-	return bind::parameter(ring_modulator::g_plugin->get_parameter(index));
+	return bind::parameter(g_plugin->get_parameter(index));
 }
 
 blink_Parameter blink_get_parameter_by_uuid(blink_UUID uuid)
 {
-	return bind::parameter(ring_modulator::g_plugin->get_parameter_by_uuid(uuid));
+	return bind::parameter(g_plugin->get_parameter_by_uuid(uuid));
 }
 
 const char* blink_get_error_string(blink_Error error)
 {
 	return blink::get_std_error_string(blink_StdError(error));
+}
+
+CMRC_DECLARE(ring_modulator);
+
+blink_ResourceData blink_get_resource_data(const char* path)
+{
+	if (g_plugin->resources().has(path)) return g_plugin->resources().get(path);
+
+	const auto fs = cmrc::ring_modulator::get_filesystem();
+
+	if (!fs.exists(path)) return { 0, 0 };
+	if (!fs.is_file(path)) return { 0, 0 };
+
+	const auto file = fs.open(path);
+
+	return g_plugin->resources().store(path, file);
 }
