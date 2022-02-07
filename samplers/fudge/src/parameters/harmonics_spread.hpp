75d2b27cd8d6a8f0b5aaa5a7caaf415ba636c085
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-02-07 00:28:20 +0000

    fudge: harmonics spread shouldn't be default active

--- samplers/fudge/src/parameters/harmonics_spread.hpp
+++ samplers/fudge/src/parameters/harmonics_spread.hpp
@@ -73,7 +73,6 @@ auto inline envelope_parameter()
 	out.clamp_range = { 0.0f, 10.0f };
 
 	out.flags |= blink_EnvelopeFlags_HostClamp;
-	out.flags |= blink_EnvelopeFlags_DefaultActive;
 	out.flags |= blink_EnvelopeFlags_CanManipulate;
 	out.flags |= blink_EnvelopeFlags_IsManipulatorTarget;
 
