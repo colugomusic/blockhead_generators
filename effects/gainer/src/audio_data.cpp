0fa04c21cb6fcf3610f8d56715e74ae89c07b41a
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-04-10 19:54:10 +0100

    gainer: copy paste error

--- effects/gainer/src/audio_data.cpp
+++ effects/gainer/src/audio_data.cpp
@@ -4,7 +4,7 @@ namespace gainer {
 
 AudioData::Envelopes::Envelopes(const Parameters& parameters, const blink_ParameterData* parameter_data)
 	: amp { parameters.env.amp->envelope, parameter_data }
-	, pan { parameters.env.amp->envelope, parameter_data }
+	, pan { parameters.env.pan->envelope, parameter_data }
 {
 }
 
