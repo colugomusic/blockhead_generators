e48feba006490234a08da0d26ba14721fb358464
Colugo <colugomusic@gmail.com>
2024-06-02 17:53:25 +0100

    Update tract_params.hpp

--- shared/tract_params.hpp
+++ shared/tract_params.hpp
@@ -46,7 +46,7 @@ auto add_param_env_throat_diameter(const blink::Plugin& plugin) -> blink_ParamId
 auto add_param_env_throat_position(const blink::Plugin& plugin) -> blink_ParamIdx {
 	const auto param_idx = blink::add::param::env(plugin, {"64e246db-0d4d-4dc2-9d34-78a3a23070bc"});
 	const auto env_idx   = blink::add::env::percentage_bipolar(plugin.host);
-	const auto flags    = blink_ParamFlags_DefaultActive | blink_ParamFlags_CanManipulate | blink_ParamFlags_HostClamp;
+	const auto flags     = blink_ParamFlags_DefaultActive | blink_ParamFlags_CanManipulate | blink_ParamFlags_HostClamp;
 	blink::write::param::name(plugin, param_idx, {"Throat Position"});
 	blink::write::param::short_name(plugin, param_idx, {"Position"});
 	blink::write::param::group(plugin, param_idx, {"Throat"});
@@ -60,29 +60,17 @@ auto add_param_env_throat_position(const blink::Plugin& plugin) -> blink_ParamId
 auto add_param_env_fricative_intensity(const blink::Plugin& plugin) -> blink_ParamIdx {
 	const auto param_idx = blink::add::param::env(plugin, {"53c94f7f-d6b7-497e-8deb-9acd35f0d60e"});
 	const auto env_idx   = blink::add::env::percentage(plugin.host);
+	const auto flags     = blink_ParamFlags_CanManipulate | blink_ParamFlags_HostClamp;
 	blink::write::env::default_value(plugin, env_idx, 1.0f);
+	blink::write::param::name(plugin, param_idx, {"Fricative Intensity"});
+	blink::write::param::short_name(plugin, param_idx, {"Intensity"});
 	blink::write::param::group(plugin, param_idx, {"Fricatives"});
-	// TODO:
+	blink::write::param::env(plugin, param_idx, env_idx);
+	blink::write::param::offset_env(plugin, param_idx, blink::add::env::percentage_bipolar(plugin.host));
+	blink::write::param::override_env(plugin, param_idx, env_idx);
+	blink::write::param::clamp_range(plugin, param_idx, {0.0f, 1.0f});
+	blink::write::param::add_flags(plugin, param_idx, flags);
 	return param_idx;
-
-	//inline auto envelope_parameter()
-	//{
-	//	blink::EnvelopeParameterSpec out;
-
-	//	out.uuid = UUID;
-	//	out.name = "Fricative Intensity";
-	//	out.short_name = "Intensity";
-	//	out.envelope = envelope();
-	//	out.offset_envelope = blink::std_params::percentage::bipolar::envelope();
-	//	out.override_envelope = envelope();
-
-	//	out.clamp_range = {0.0f, 1.0f};
-
-	//	out.flags |= blink_ParamFlags_CanManipulate;
-	//	out.flags |= blink_ParamFlags_HostClamp;
-
-	//	return out;
-	//}
 }
 
 } // tract
