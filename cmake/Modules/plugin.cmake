9565e6234d96dd9b7d6b6f0e204a055e0143b046
Colugo <colugomusic@gmail.com>
2025-02-12 12:42:23 +0000

    Use submodule for blink instead of CPM

--- cmake/Modules/plugin.cmake
+++ cmake/Modules/plugin.cmake
@@ -1,14 +1,5 @@
-include(CPM)
-
-CPMAddPackage(
-	NAME              blink
-	GITHUB_REPOSITORY colugomusic/blink
-	GIT_TAG           master
-	DOWNLOAD_ONLY     YES
-)
-
 if (NOT TARGET blink_plugin)
-  add_subdirectory(${blink_SOURCE_DIR}/lib/plugin blink_plugin)
+	add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../../extern/submodules/blink/lib/plugin blink_plugin)
 endif()
 
 source_group(src REGULAR_EXPRESSION "/src/.*\\.(h|cpp)")
